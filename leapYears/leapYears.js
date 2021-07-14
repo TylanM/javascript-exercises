b4ac3fa86204173cb3252a2fbfac7e461ab6e4fd
tylanm <tylanmcpherson@gmail.com>
2021-07-13 18:08:54 -0700

    passes all test cases

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(yearToEvaluate) {
+    if (yearToEvaluate % 4 != 0){
+        return false;
+    } else if (yearToEvaluate % 100 == 0 && yearToEvaluate % 400 !=0){
+        return false;
+    } else {
+        return true;
+    }
 };
+
 
 module.exports = leapYears;
